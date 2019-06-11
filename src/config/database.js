f890493a4f4bcdb24b5d72e44b6b4eef4e5bffd4
marcosferrare <marcosferrare@DTI02>
2019-06-11 16:35:54 -0300

    Exemplo de connection

--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,4 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect('mongodb://localhost/todo')
+//module.exports = mongoose.connect('mongodb://Kako:fcm020964@localhost/todo')
